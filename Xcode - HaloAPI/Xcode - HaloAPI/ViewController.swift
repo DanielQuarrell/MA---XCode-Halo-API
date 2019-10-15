63892a08d3a73bc545b2b28f386b2c367ba8a783
Daniel Quarrell <DanielQuarrell@yahoo.co.uk>
2019-10-15 16:54:02 +0100

    Blank Xcode project

---
+++ Xcode - HaloAPI/Xcode - HaloAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xcode - HaloAPI
+//
+//  Created by Daniel Quarrell on 15/10/2019.
+//  Copyright © 2019 Daniel Quarrell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
