843a204e83c720a1b7c78dbda8655ee4e39cbf6f
Daniel Quarrell <DanielQuarrell@yahoo.co.uk>
2019-10-16 00:48:41 +0100

    Added CocoaPods and Alamofire to the application

---
+++ Xcode - HaloAPI/Pods/Target Support Files/Pods-Xcode - HaloAPITests/Pods-Xcode - HaloAPITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Xcode___HaloAPITests : NSObject
+@end
+@implementation PodsDummy_Pods_Xcode___HaloAPITests
+@end
---
+++ Xcode - HaloAPI/Pods/Target Support Files/Pods-Xcode - HaloAPIUITests/Pods-Xcode - HaloAPIUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Xcode___HaloAPIUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Xcode___HaloAPIUITests
+@end
