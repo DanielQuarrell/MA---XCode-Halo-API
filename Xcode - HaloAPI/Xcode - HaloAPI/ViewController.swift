723001d5e6114ca3974102d49c9fa639ef62bfec
Daniel Quarrell <DanielQuarrell@yahoo.co.uk>
2019-10-15 17:29:10 +0100

    Fetched data with a GET query and printed it to the console
    - Use URLSession.shared as a singleton
    - Create a data task (Resume it)
    - Handle data/error in closure

--- Xcode - HaloAPI/Xcode - HaloAPI/ViewController.swift
+++ Xcode - HaloAPI/Xcode - HaloAPI/ViewController.swift
@@ -14,6 +14,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        fetchURL(url: "http://orangevalleycaa.org/api/videos.php")
+    }
+    
+    func fetchURL(url : String) {
+        if let urlToServer = URL.init(string: url) {
+            let task = URLSession.shared.dataTask(with: urlToServer, completionHandler: { (data, response, error) in
+                if error != nil || data == nil {
+                    //Handle error
+                }
+                else {
+                    if let respString = String.init(data: data!, encoding: .ascii) {
+                        print(respString)
+                    }
+                }
+            })
+            task.resume()
+        }
+    }
 
 
 }
