c5b75bb1585e8eed097d18188213c9dd7e0d459f
DanielQuarrell <Daniel2.Quarrell@live.uwe.ac.uk>
2019-10-17 21:28:07 +0100

    Got spartan image to load into an image view using parameters and headers

--- Xcode - HaloAPI/Xcode - HaloAPI/ViewController.swift
+++ Xcode - HaloAPI/Xcode - HaloAPI/ViewController.swift
@@ -11,6 +11,8 @@ import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var spartanImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,14 +20,18 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        fetchURL(url: "http://orangevalleycaa.org/api/videos.php")
+        fetchURL(url: "https://www.haloapi.com/profile/h5/profiles/Danny%20Q%2077/spartan", param: ["size" : 512, "crop" : "full"])
     }
     
-    func fetchURL(url : String) {
-        AF.request(url).responseString{ (response) in
-            print (response.value ?? "no value")
-        }.responseJSON{ (response) in
-            print (response.value ?? "no value")
+    func fetchURL(url : String, param : [String:Any]) {
+        let headers : HTTPHeaders = [
+            "Ocp-Apim-Subscription-Key" : "0bf9c5f6ddc64f7c8e34262bfd326b33"
+        ]
+        
+        AF.request(url, parameters: param, headers: headers).responseData{ (response) in
+            debugPrint(response)
+            let img = UIImage.init(data: response.data!)
+            self.spartanImage.image = img
         }
     }
 
