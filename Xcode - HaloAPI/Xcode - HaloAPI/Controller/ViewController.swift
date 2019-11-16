dfa124dcab28cd6cbcea771a413c0212fd52b8f8
DanielQuarrell <Daniel2.Quarrell@live.uwe.ac.uk>
2019-11-16 19:27:14 +0000

    Encapsulated Network Calls into its own class called networking layer, implemented completion calls

--- Xcode - HaloAPI/Xcode - HaloAPI/Controller/ViewController.swift
+++ Xcode - HaloAPI/Xcode - HaloAPI/Controller/ViewController.swift
@@ -17,8 +17,6 @@ class ViewController: UIViewController {
     
     var gamertagString : String = ""
     
-    let haloApi = HaloApiInterface()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,27 +26,6 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
     }
-    
-    /*
-    func fetchImageURL(url: String, param: [String:Any], imageView: UIImageView) {
-        let headers : HTTPHeaders = [
-            "Ocp-Apim-Subscription-Key" : "0bf9c5f6ddc64f7c8e34262bfd326b33"
-        ]
-        
-        AF.request(url, parameters: param, headers: headers).responseData{ (response) in
-            debugPrint(response)
-            
-            switch response.result{
-            case .success :
-                let img = UIImage.init(data: response.data!)
-                imageView.image = img
-                
-            case .failure(let error):
-                print("Error : \(error)" )
-            }
-        }
-    }
-     */
     
     func validateGamertag(gamertag: String){
         let url : String = "https://www.haloapi.com/profile/h5/profiles/" + gamertag + "/appearance"
@@ -93,11 +70,23 @@ class ViewController: UIViewController {
     @IBAction func checkStatsButtonPressed(_ sender: Any) {
         
         gamertagField.resignFirstResponder()
-        let gamertag : String = gamertagField.text!.replacingOccurrences(of: " ", with: "%20")
+        let gamertag : String = gamertagField.text!
         
-        validateGamertag(gamertag: gamertag)
+        HaloApiInterface.sharedInstance.validateGamertag(gamertag: gamertag){ (isValid) in
+            if isValid
+            {
+                HaloApiInterface.sharedInstance.fetchSpartanImage(param: ["size" : 512, "crop" : "full"]){ (image) in
+                    self.spartanImage.image = image
+                }
+                //Continue to next page
+            }
+            else
+            {
+                self.createValidationAlertBox()
+            }
+        }
         
-        haloApi.fetchImageURL(url: "https://www.haloapi.com/profile/h5/profiles/" + gamertag + "/spartan", param: ["size" : 512, "crop" : "full"], imageView: self.spartanImage)
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
--- Xcode - HaloAPI/Xcode - HaloAPI/Model/NetworkLayer.swift
+++ Xcode - HaloAPI/Xcode - HaloAPI/Model/NetworkLayer.swift
@@ -12,14 +12,54 @@ import SwiftyJSON
 
 class HaloApiInterface
 {
+    static let sharedInstance = HaloApiInterface()
+    
     var playerName : String = ""
     
     let baseURL : String = "https://www.haloapi.com/profile/h5/"
+    let profileURL : String = "profiles/"
+    let serviceRecordURL : String = "servicerecords/"
+    let arenaURL : String = "arena?players="
+    let warzoneURL : String = "warzone?players="
     
-    func fetchImageURL(url: String, param: [String:Any], imageView: UIImageView) {
-        let headers : HTTPHeaders = [
-            "Ocp-Apim-Subscription-Key" : "0bf9c5f6ddc64f7c8e34262bfd326b33"
-        ]
+    let headers : HTTPHeaders = [
+        "Ocp-Apim-Subscription-Key" : "0bf9c5f6ddc64f7c8e34262bfd326b33"
+    ]
+    
+    func validateGamertag(gamertag: String, completion: @escaping (_ isValid: Bool) -> ()){
+        let url : String = baseURL + "profiles/" + gamertag.replacingOccurrences(of: " ", with: "%20") + "/appearance"
+        
+        AF.request(url, headers: headers).responseJSON{ (response) in
+            debugPrint(response)
+            
+            switch response.result{
+            case .success(let value) :
+                if(response.response?.statusCode != 404)
+                {
+                    let json = JSON(value)
+                    let playerNameFromJson = json["Gamertag"].stringValue
+                    print("Gamertag is valid:" + playerNameFromJson)
+                    
+                    self.playerName = playerNameFromJson.replacingOccurrences(of: " ", with: "%20")
+                    
+                    completion(true)
+                }
+                else
+                {
+                    print("Please enter valid gamertag")
+                    completion(false)
+                }
+                
+            case .failure(let error):
+                print("Error : \(error)" )
+                completion(false)
+            }
+        }
+    }
+    
+    func fetchImage(imageURL: String, param: [String:Any], completion: @escaping (_ image: UIImage) -> ())
+    {
+        let url = baseURL + imageURL
         
         AF.request(url, parameters: param, headers: headers).responseData{ (response) in
             debugPrint(response)
@@ -27,11 +67,31 @@ class HaloApiInterface
             switch response.result{
             case .success :
                 let img = UIImage.init(data: response.data!)
-                imageView.image = img
+                completion(img!)
                 
             case .failure(let error):
                 print("Error : \(error)" )
             }
+        }
+    }
+    
+    func fetchSpartanImage(param: [String:Any], completion: @escaping (_ spartanImage: UIImage) -> ())
+    {
+        let imageURL : String = profileURL + playerName + "/spartan"
+        //www.haloapi.com/profile/h5/profiles/{player}/spartan[?size][&crop]
+        
+        fetchImage(imageURL: imageURL, param: param) { (spartanImage) in
+            completion(spartanImage)
+        }
+    }
+    
+    func fetchEmblemImage(param: [String:Any], completion: @escaping (_ emblemImage: UIImage) -> ())
+    {
+        let imageURL : String = profileURL + self.playerName + "/emblem"
+        //www.haloapi.com/profile/h5/profiles/{player}/spartan[?size][&crop]
+        
+        fetchImage(imageURL: imageURL, param: param) { (emblemImage) in
+            completion(emblemImage)
         }
     }
 }
